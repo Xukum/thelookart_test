6825b0a6d0a964d55e843083b750b998c170c458
xukum <xukum@localhost.localdomain>
2012-12-18 22:37:21 -0500

    ewrwe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test3::Application.config.secret_token = 'f30bc1972a2d8e63cc81f9b1156f16ea996e3f29bdba813d1b9320e9089d08427188c47aba6669482ad46f9ee38c5718aff82d94e29f60147ae3791474b490d2'
